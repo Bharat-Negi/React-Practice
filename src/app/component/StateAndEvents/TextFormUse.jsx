387685eea1609bc56e5c75f8aa91de7cf6f97ca8
Bharat Negi <bharat.negi@rethinkfirst.com>
2024-10-01 17:51:23 +0530

    file up

--- src/app/component/StateAndEvents/TextFormUse.jsx
+++ src/app/component/StateAndEvents/TextFormUse.jsx
@@ -9,19 +9,42 @@ const TextFormUse = (props) => {
         setText(newText)
     }
 
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleClearClick = () => {
+        let newText = '';
+        setText(newText)
+    }
+
     const handleOnChange = (event) => {
         setText(event.target.value)
     }
 
   return (
-    <div className='mt-5 container'>
-        <h4>{props.heading}</h4>
-        <div className="form-group mb-3">
-            <label className='mb-2'>{props.labelName}</label>
-            <textarea className="form-control" value={text} onChange={handleOnChange}  rows="8"></textarea>
+    <>
+        <div className='mt-5 container'>
+            <h4>{props.heading}</h4>
+            <div className="form-group mb-3">
+                <label className='mb-2'>{props.labelName}</label>
+                <textarea className="form-control" value={text} onChange={handleOnChange}  rows="6"></textarea>
+            </div>
+            <div className='d-flex gap-2'>
+                <button type="button" class="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button type="button" class="btn btn-success" onClick={handleLowClick}>Convert to Lowercase</button>
+                <button type="button" class="btn btn-danger" onClick={handleClearClick}>Clear Text</button>
+            </div>            
         </div>
-        <button type="button" class="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-    </div>
+        <div className='mt-3 container'>
+            <h4>Heading</h4>
+            <p>{text.split(" ").length} Words, {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes Read</p>
+            <h5>Preview</h5>
+            <p>{text}</p>
+        </div>
+    </>
   )
 }
 
