fe33b8c33eb513c72216d3a7f1bd36d152ba9439
Bharat Negi <bharat.negi@rethinkfirst.com>
2024-10-01 19:14:19 +0530

    text update

--- src/app/component/StateAndEvents/TextFormUse.jsx
+++ src/app/component/StateAndEvents/TextFormUse.jsx
@@ -23,24 +23,32 @@ const TextFormUse = (props) => {
         setText(event.target.value)
     }
 
+    const wordMin = 0.008 * text.split(" ").length;
+
   return (
     <>
         <div className='mt-5 container'>
             <h4>{props.heading}</h4>
             <div className="form-group mb-3">
                 <label className='mb-2'>{props.labelName}</label>
-                <textarea className="form-control" value={text} onChange={handleOnChange}  rows="6"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} rows="6"></textarea>
             </div>
             <div className='d-flex gap-2'>
-                <button type="button" class="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button type="button" class="btn btn-success" onClick={handleLowClick}>Convert to Lowercase</button>
-                <button type="button" class="btn btn-danger" onClick={handleClearClick}>Clear Text</button>
+                <button type="button" class="btn btn-primary" onClick={handleUpClick}>
+                    Convert to Uppercase
+                </button>
+                <button type="button" class="btn btn-success" onClick={handleLowClick}>
+                    Convert to Lowercase
+                </button>
+                <button type="button" class="btn btn-danger" onClick={handleClearClick}>
+                    Clear Text
+                </button>
             </div>            
         </div>
         <div className='mt-3 container'>
             <h4>Heading</h4>
             <p>{text.split(" ").length} Words, {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes Read</p>
+            <p>{wordMin.toFixed(3)} Minutes Read</p>
             <h5>Preview</h5>
             <p>{text}</p>
         </div>
